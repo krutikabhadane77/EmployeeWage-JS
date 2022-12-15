baee0646c4791a8b2ff8d79cb27d7ec62a19c34f
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-12-15 09:03:43 +0530

    Added: UC4 Calculating wages for a month

--- EmployeeWage/EmployeeWage.js
+++ EmployeeWage/EmployeeWage.js
@@ -15,8 +15,15 @@ function getWorkingHours(empCheck) {
     }
 }
 
-let empCheck = Math.floor(Math.random()*10)%3;
-let empHrs = getWorkingHours(empCheck);
-let empWage = empHrs*WAGE_PER_HOUR;
-console.log("Emp hours: "+empHrs);
+const NUM_OF_WORKING_DAYS = 2;
+
+let totalEmpHrs = 0;
+for(day=0;day<NUM_OF_WORKING_DAYS;day++){
+    let empCheck = Math.floor(Math.random()*10)%3;
+    totalEmpHrs = totalEmpHrs+getWorkingHours(empCheck);
+}
+
+let empWage = totalEmpHrs*WAGE_PER_HOUR;
+
+console.log("Emp hours: "+totalEmpHrs);
 console.log("Emp wage: "+empWage)
