422e7146abe0a403466974536899b97f37b689b5
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-12-18 23:22:44 +0530

    Added: UC10 Ability to store the day hours worked and wage earned in a single object

--- EmployeeWage/EmployeeWage.js
+++ EmployeeWage/EmployeeWage.js
@@ -28,6 +28,7 @@ let empDailyWageArray = new Array();
 let empDailyWageMap = new Map();
 let empDailyHrsMap = new Map();
 
+let empDailyHrsAndWageArr = new Array();
 while((totalEmpHrs<=MAX_HRS_IN_MONTH) && 
         (totalWorkingDays<NUM_OF_WORKING_DAYS)){
     totalWorkingDays++;
@@ -37,7 +38,19 @@ while((totalEmpHrs<=MAX_HRS_IN_MONTH) &&
     empDailyWageArray.push(calcDailyWage(empHrs));
     empDailyWageMap.set(totalWorkingDays,calcDailyWage(empHrs));
     empDailyHrsMap.set(totalWorkingDays,empHrs);
+
+
+empDailyHrsAndWageArr.push(
+    {
+        dayNum : totalWorkingDays,
+        dailyHours : empHrs,
+        dailyWage : calcDailyWage(empHrs),
+        toString(){
+            return "Day=" + this.dayNum+": Working Hours is "+this.dailyHours+"And wage Earned "+this.dailyWage+"\n"
+        },
+    });
 }
+console.log("Daily hours worked and wage earned: "+empDailyHrsAndWageArr);
 
 console.log("Daily Wage Array: "+empDailyWageArray);
 console.log("Daily Wage Map:");
@@ -120,8 +133,6 @@ console.log("Number of Days Emp Worked: "+
     empDailyWageArray.reduce(totalDaysWorked,0));
 
 //UC 9
-console.log("UC 9");
-
 const findTotal=(TotalVal, dailyVal) => {return TotalVal+dailyVal};
 let count=0;
 let totalHours=Array.from(empDailyHrsMap.values()).reduce(findTotal,0);
@@ -140,3 +151,4 @@ empDailyHrsMap.forEach((value,key,map)=>{
 console.log("Full working days: "+fullWorkingDays);
 console.log("Part working days: "+partWorkingDays);
 console.log("Non working days: "+nonWorkingDays);
+
